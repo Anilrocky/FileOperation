947d7f0cbb7ed613f30a5b8a0c699d98ba337107
Anilrocky <anilkumarac96@gmail.com>
2023-04-12 12:12:30 +0530

    [Anil] Add:Check file exists or not

---
+++ FileOperation/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperation
+{
+    public class Operation
+    {
+        public void FileExists(string filePath)
+        {
+            if (File.Exists(filePath))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine("File not exists");
+        }
+    }
+}
--- FileOperation/Program.cs
+++ FileOperation/Program.cs
@@ -5,7 +5,22 @@ namespace FileOperation
     {
         static void Main(string[] args)
         {
-
+            Operation operation = new Operation();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Check File exists \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        operation.FileExists(@"D:\BridgeLabz\FileOperation\FileOperation\File.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
